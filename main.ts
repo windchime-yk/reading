a1a34dc0a195ebe7ed1d42ebeb4f10d719d1084d
WhyK <windchimeyk@gmail.com>
2022-07-12 21:54:39 +0900

    feat: add deno_blog

---
+++ main.ts
@@ -0,0 +1,14 @@
+import blog from "https://deno.land/x/blog@0.4.0/blog.tsx";
+
+blog({
+  title: "WhyK Reading",
+  author: "WhyK",
+  description: "技術書について書き残したいことを書きます",
+  avatar: "./windchime2.jpg",
+  avatarClass: "full",
+  favicon: "./windchime2.jpg",
+  links: [
+    { title: "Twitter", url: "https://twitter.com/windchime_yk" },
+    { title: "GitHub", url: "https://github.com/windchime-yk" },
+  ],
+});
